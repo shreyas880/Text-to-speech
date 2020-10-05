106ca3f02041b38fe07fa904d8b9bceb2265d6ec
shreyas880 <62434709+shreyas880@users.noreply.github.com>
2020-10-05 20:59:29 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -16,13 +16,63 @@ export default class App extends React.Component {
     }
   }
 
-  convertText(){
-    var textToConvert = this.state.text
-    Speech.speak(textToConvert)
+  convertText(text){
+    Speech.speak(text)
   }
 
   render(){
-      return (
+    if(this.state.ishistoryButtonPressed === 'true'){
+      return(
+        <View style={styles.container}>
+        <Header backgroundColor={'#9c8210'} centerComponent={{text: 'Text To Speech App ',style: { color: '#fff', fontSize: 20 }}}/>
+        <Text style={styles.Text}>Please type your text here↓</Text>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({ text: text });
+          }}
+          placeholder="Enter your word here"
+          value={this.state.text}
+        />
+        <TouchableOpacity style={styles.goButton} 
+        onPress={() => {
+          this.state.text === ''?
+            alert('You have not entered any text!Please enter text and try again.'):
+              this.convertText(this.state.text);
+
+          this.state.history.push(this.state.text);
+
+          console.log(this.state.history)
+          
+          console.log(this.state.text)
+
+        }}><Text style={styles.buttonText}>Search</Text></TouchableOpacity>
+        <Text style={styles.Text}>The sentence that has been typed:{this.state.text}</Text>
+        {this.state.history.map((word) => (
+          <TouchableOpacity style={{
+            marginLeft:65,
+            width:200,
+            height:50,
+            justifyContent:'center',
+            alignItems:'center',
+            borderRadius:50,
+            borderWidth:1,
+            borderColor:'black',
+            marginTop:50,
+            backgroundColor:'blue'
+          }}
+            onPress={()=>{
+              this.state.text = {word}  
+              this.convertText(word)     
+            }}>
+              <Text style={{color:'black',fontWeight:'bold',fontSize:22}}>{word}</Text>
+            </TouchableOpacity>
+        ))}
+
+      </View>
+    );
+  }else{
+    return (
       <View style={styles.container}>
         <Header backgroundColor={'#9c8210'} centerComponent={{text: 'Text To Speech App ',style: { color: '#fff', fontSize: 20 }}}/>
         <Text style={styles.Text}>Please type your text here↓</Text>
@@ -39,11 +89,27 @@ export default class App extends React.Component {
           this.state.text === ''?
             alert('You have not entered any text!Please enter text and try again.'):
               this.convertText(this.state.text);
+
+          this.state.history.push(this.state.text);
+
+          console.log(this.state.history)
+          
           console.log(this.state.text)
+
         }}><Text style={styles.buttonText}>Search</Text></TouchableOpacity>
-        <Text style={styles.Text}>The word that has been typed:{this.state.text}</Text>
+        <Text style={styles.Text}>The sentence that has been typed:{this.state.text}</Text>
+        <TouchableOpacity style={styles.goButton}
+        onPress={()=>{
+          this.state.ishistoryButtonPressed = 'true'
+          console.log(this.state.ishistoryButtonPressed)
+
+        }}>
+          <Text style={styles.buttonText}>Show History</Text>
+        </TouchableOpacity>
       </View>
-    );  
+    );
+  }
+        
   }
 }
 
@@ -76,7 +142,7 @@ const styles = StyleSheet.create({
     backgroundColor:'red'
   },
   buttonText: {
-    fontSize:32,
+    fontSize:26,
     fontWeight:'bold',
     color:'black'
   },
